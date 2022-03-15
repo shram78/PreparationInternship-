1d97aacd2e1115207c296c7d603beff37c82081b
Aleksandr <ra00275@gmail.com>
2022-03-15 14:06:02 +0300

    Add move player, change gitIgnore

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -11,48 +11,32 @@ public class PlayerMover : MonoBehaviour
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
-   // [SerializeField] private float _inertionX;
     [SerializeField] private AudioSource _jumpSound;
-    [SerializeField] private Player _player;
-    [SerializeField] private ParticleSystem _playerDie;
 
     private Rigidbody2D _rigidbody2D;
     private bool _isGrounded;
-    private Animator _animator;
-    private const string IsJumping = "IsJumping";
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-
-        _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        Move1();
-        _isGrounded = Physics2D.OverlapCircle(_groundChecker.position, 0.1f, _groundLayer);
+         _isGrounded = Physics2D.OverlapCircle(_groundChecker.position, 0.1f, _groundLayer);
 
-        _animator.SetBool(IsJumping, false);
+        Move();
 
-            if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
-                //Move1();
-                Jump1();
-
-        //    if (_isGrounded)
-          //      Jump1();
+        if (Input.GetKey(KeyCode.Space) && _isGrounded)
+        Jump();
     }
 
-    private void Move1()
+    private void Move()
     {
-        _animator.SetBool(IsJumping, true);
-
         transform.Translate(Vector2.right * _speed * Time.deltaTime);
-
-      //  _rigidbody2D.AddForce(new Vector2(_inertionX, 0) * Time.deltaTime, ForceMode2D.Force);
     }
 
-    private void Jump1()
+    private void Jump()
     {
         _jumpSound.Play();
 
