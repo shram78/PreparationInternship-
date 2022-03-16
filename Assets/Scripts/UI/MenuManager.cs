6d84573ecb75ac87d181d063efea6ccdebf2d304
Aleksandr <ra00275@gmail.com>
2022-03-16 18:33:55 +0300

    add coin

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -4,13 +4,8 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private Player _player;
-    [SerializeField] private AudioSource _buttonClickSound;
-    [SerializeField] private Button _openStoreButton;
-
     private void OnBackMainMenuButton()
     {
-        //Time.timeScale = 1;
         MainMenu.Load();
     }
 }
