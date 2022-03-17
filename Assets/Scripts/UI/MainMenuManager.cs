588f4de975a7e7ee8ee8f75f945e70abdd62dfb4
Aleksandr <ra00275@gmail.com>
2022-03-17 16:47:29 +0300

    create main menu

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -3,25 +3,47 @@ using IJunior.TypedScenes;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Animator))]
+
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] private AudioSource _buttonClickSound;
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _exitButton;
+    [SerializeField] private Button _backMainMenuButton;
+
+    [SerializeField] private Button _creatorButton;
+    [SerializeField] private AudioSource _buttonClickSound;
     [SerializeField] private Button _iJuniorUrlButton;
+
+    private Animator _animator;
+    private const string _isButtonPress = "IsButtonPressed";
 
     private void OnEnable()
     {
         _playButton.onClick.AddListener(OnPlayButtonClick);
         _exitButton.onClick.AddListener(OnExitButtonClick);
+        _creatorButton.onClick.AddListener(OnCreatorButtomClick);
+        _backMainMenuButton.onClick.AddListener(OnBackMainMenuButton);
         _iJuniorUrlButton.onClick.AddListener(OnIJuniorButtonClick);
+    }
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
     }
 
     private void OnDisable()
     {
         _playButton.onClick.RemoveListener(OnPlayButtonClick);
         _exitButton.onClick.RemoveListener(OnExitButtonClick);
+        _creatorButton.onClick.RemoveListener(OnCreatorButtomClick);
+        _backMainMenuButton.onClick.AddListener(OnBackMainMenuButton);
         _iJuniorUrlButton.onClick.RemoveListener(OnIJuniorButtonClick);
+    }
+
+    private void OnBackMainMenuButton()
+    {
+        _animator.SetBool(_isButtonPress, false);
     }
 
     private void OnPlayButtonClick()
@@ -37,6 +59,11 @@ public class MainMenuManager : MonoBehaviour
     private void OnIJuniorButtonClick()
     {
         Application.OpenURL("https://ijunior.ru/");
+    }
+
+    private void OnCreatorButtomClick()
+    {
+        _animator.SetBool(_isButtonPress, true);
     }
 
     private IEnumerator DelayStart()
